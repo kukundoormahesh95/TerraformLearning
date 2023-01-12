45ec31adfd5d7550d11804716e5e3c49cb0262cd
kukundoormahesh95 <60536242+kukundoormahesh95@users.noreply.github.com>
2023-01-12 16:03:58 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,3 +1,15 @@
 provider "aws" {
-  region='ap-southeast-1'
+  region="ap-southeast-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+terraform {
+  backend "remote" {
+      organization = "Mahesh-Terraform-Learning"
+      workspaces {
+        name="TerraformLearning"
+      }
+
+  }
+
 }
